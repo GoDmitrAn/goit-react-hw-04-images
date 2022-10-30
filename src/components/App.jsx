6dc7cc127c7b6b28f8fcc7694fbb7ce1d8b4e803
Dmytro Holombytsia <iqbitsak@gmail.com>
2022-10-30 10:32:58 +0200

    axios and form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,22 @@
 import { Searchbar } from './Searchbar/Searchbar';
+import axios from 'axios';
+import { Component } from 'react';
 
-export const App = () => {
-  return (
-    <div>
-      <Searchbar />
-    </div>
-  );
-};
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+axios.defaults.headers.common['x-api-key'] = process.env.REACT_APP_API_KEY;
+export class App extends Component {
+  state = {
+    search: '',
+  };
+  formSubmitHandler = data => {
+    this.setState({ search: data });
+    console.log(`'app-data'`, data);
+  };
+  render() {
+    return (
+      <div>
+        <Searchbar onSubmitForm={this.formSubmitHandler} />
+      </div>
+    );
+  }
+}
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,22 +1,30 @@
 import IconButton from '@mui/material/IconButton';
 import ImageSearchIcon from '@mui/icons-material/ImageSearch';
+import { Component } from 'react';
 
-export const Searchbar = () => {
-  return (
-    <div className="Searchbar">
-      <form className="SearchForm">
-        <label className="SearchForm-button-label">
-          <IconButton type="submit" aria-label="image search icon">
-            <ImageSearchIcon />
-          </IconButton>
-          <input
-            type="text"
-            name="name"
-            className="SearchForm-input"
-            placeholder="Search images and photos"
-          />
-        </label>
-      </form>
-    </div>
-  );
-};
+export class Searchbar extends Component {
+  handleSubmit = event => {
+    event.preventDefault();
+    const userSearch = event.target.elements.search.value;
+    this.props.onSubmitForm(userSearch);
+  };
+  render() {
+    return (
+      <div className="Searchbar">
+        <form className="SearchForm" onSubmit={this.handleSubmit}>
+          <label className="SearchForm-button-label">
+            <IconButton type="submit" aria-label="image search icon">
+              <ImageSearchIcon />
+            </IconButton>
+            <input
+              type="text"
+              name="search"
+              className="SearchForm-input"
+              placeholder="Search images and photos"
+            />
+          </label>
+        </form>
+      </div>
+    );
+  }
+}
