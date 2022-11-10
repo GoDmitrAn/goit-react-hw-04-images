b00d7d596a085694d59ec3cfa83287f549c9820f
Dmytro Holombytsia <iqbitsak@gmail.com>
2022-11-10 13:51:41 +0200

    app on hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ import { useEffect } from 'react';
 import { useRef } from 'react';
 
 axios.defaults.baseURL = 'https://pixabay.com/api';
-
+let imageOnModal;
 export const App = () => {
   const [search, setSearch] = useState(null);
   const [searchGallery, setSearchGallery] = useState([]);
@@ -18,18 +18,12 @@ export const App = () => {
   const [error, setError] = useState(null);
   const [page, setPage] = useState(1);
   const [showModal, setShowModal] = useState(false);
+
   const galleryPages = useRef(1);
+  useEffect(() => {
+    setSearchGallery([]);
+  }, []);
 
-  // state = {
-  //   search: null,
-  //   searchGallery: null,
-  //   loading: false,
-  //   error: null,
-  //   page: 1,
-  //   showModal: false,
-  // };
-
-  let imageOnModal = null;
   useEffect(() => {
     if (!search) {
       console.log('no-search');
@@ -46,145 +40,35 @@ export const App = () => {
             per_page: 12,
           },
         });
-        setSearchGallery(response.data.hits);
+        setSearchGallery(s => [...s, ...response.data.hits]);
         setLoading(false);
         if (response.data.totalHits > 12) {
           galleryPages.current = Math.ceil(response.data.totalHits / 12);
-          console.log(galleryPages.current);
         }
       } catch (error) {
         console.log(error);
-        // this.setState({ error: 'Sorry, please reload' });
+
         setError('Sorry, please reload');
       }
     }
     fetchData();
   }, [search, page, loading]);
-  // useEffect(() => {
-  //   if (!search) {
-  //     console.log('no-search');
-  //     return;
-  //   }
 
-  //   async function fetchData() {
-  //     try {
-  //       const response = await axios.get('/?', {
-  //         params: {
-  //           key: process.env.REACT_APP_API_KEY,
-  //           q: search,
-  //           page: page,
-  //           per_page: 12,
-  //         },
-  //       });
-
-  //       if (response.data.total === 0) {
-  //         // this.setState({ searchGallery: null, loading: false });
-  //         setLoading(false);
-  //         setSearchGallery(null);
-  //         return;
-  //       }
-  //       if (response.data.totalHits > 12) {
-  //         galleryPages.current = Math.ceil(response.data.totalHits / 12);
-  //         console.log(galleryPages.current);
-  //       }
-
-  //       if (page > 1) {
-  //         // this.setState(prevState => {
-  //         //   return {
-  //         //     searchGallery: [
-  //         //       ...prevState.searchGallery,
-  //         //       ...response.data.hits,
-  //         //     ],
-  //         //     loading: false,
-  //         //   };
-  //         // });
-  //         setSearchGallery(prevState => [...prevState, ...response.data.hits]);
-  //         setLoading(false);
-  //         return;
-  //       }
-  //       // this.setState({ searchGallery: response.data.hits, loading: false });
-  //       setSearchGallery(response.data.hits);
-  //       setLoading(false);
-  //     } catch (error) {
-  //       console.log(error);
-  //       // this.setState({ error: 'Sorry, please reload' });
-  //       setError('Sorry, please reload');
-  //     }
-  //   }
-  //   fetchData();
-  // }, [search, page, searchGallery, error, loading, galleryPages]);
-
-  // async function componentDidUpdate(prevProps, prevState) {
-  //   const { search, page, searchGallery, loading } = this.state;
-  //   if (!search) {
-  //     console.log('no-search');
-  //     return;
-  //   }
-  //   if (prevState.search === search && prevState.page === page) {
-  //     return;
-  //   }
-
-  //   if (searchGallery & (this.galleryPages === 1)) {
-  //     console.log('no more photos');
-  //     return;
-  //   }
-  //   if (!loading) {
-  //     this.setState({ loading: true });
-  //   }
-  //   try {
-  //     const response = await axios.get('/?', {
-  //       params: {
-  //         key: process.env.REACT_APP_API_KEY,
-  //         q: this.state.search,
-  //         page: this.state.page,
-  //         per_page: 12,
-  //       },
-  //     });
-
-  //     if (response.data.total === 0) {
-  //       this.setState({ searchGallery: null, loading: false });
-  //       return;
-  //     }
-  //     if (response.data.totalHits > 12) {
-  //       this.galleryPages = Math.ceil(response.data.totalHits / 12);
-  //       console.log(this.galleryPages);
-  //     }
-
-  //     if (this.state.page > 1) {
-  //       this.setState(prevState => {
-  //         return {
-  //           searchGallery: [...prevState.searchGallery, ...response.data.hits],
-  //           loading: false,
-  //         };
-  //       });
-  //       return;
-  //     }
-  //     this.setState({ searchGallery: response.data.hits, loading: false });
-  //   } catch (error) {
-  //     console.log(error);
-  //     this.setState({ error: 'Sorry, please reload' });
-  //   }
-  // }
-
-  // let galleryPages = 1;
   const toggleModal = e => {
-    // this.setState(({ showModal }) => ({
-    //   showModal: !showModal,
-    // }));
     setShowModal(prevState => !prevState);
 
     if (!showModal) {
-      console.log(e.target.id);
-
       imageOnModal = searchGallery.find(
         item => item.id === Number(e.target.id)
       );
-      console.log(' imageOnModal', this.imageOnModal);
     }
   };
 
   const formSubmitHandler = data => {
     if (!data) {
+      setSearchGallery([]);
+      setSearch(null);
+      setLoading(false);
       return;
     }
     if (search === data) {
@@ -196,7 +80,7 @@ export const App = () => {
     //   page: 1,
     // });
     setSearch(data);
-    setSearchGallery(null);
+    setSearchGallery([]);
     setPage(1);
     galleryPages.current = 1;
   };
@@ -228,8 +112,7 @@ export const App = () => {
           />
         </div>
       )}
-      {page === galleryPages.current ||
-      searchGallery & (searchGallery.length < 12) ? (
+      {page === galleryPages.current || searchGallery.length < 12 ? (
         false
       ) : (
         <LoadMoreBtn onLoadMore={loadMore}>Load more</LoadMoreBtn>
